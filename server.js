a8e610cd08f2429a7cd1b2c0bfaee96e16777c32
Jesse Belcourt <jessebelcourt@gmail.com>
2020-04-14 18:56:11 -0600

    Now sending and recieving basic messages between client and server.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import ws from 'ws';
+import WebSocket from 'ws';
 
-const WebSocketServer = ws.Server;
-const wss = new WebSocketServer({port: 40510});
+const wss = new WebSocket.Server({ port: 40510 });
 
-wss.on('connection', ws => {
-  ws.on('message', message => console.log('message'));
+wss.on('open', () => {
+  console.log('connection open');
 })
+
+wss.on('connection', ws =>  {
+  ws.on('message', message => console.log(`message: ${message}`));
+  
+  setInterval(() => ws.send(`${new Date()}`), 1000);
+});
+
 
 
